32e508ea7cde139b788e211de2187f07683f4b49
Ryan Mehri <ryan.mehri1@gmail.com>
2020-03-16 11:06:42 -0700

    Connect login screen with auth

--- nutrin/lib/screens/wrapper.dart
+++ nutrin/lib/screens/wrapper.dart
@@ -10,15 +10,13 @@ import 'package:provider/provider.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-//    final user = Provider.of<User>(context);
+    final user = Provider.of<User>(context);
 
     // return either Home or Authenticate widget
-//    if (user == null) {
-//      return Authenticate();
-//    } else {
-//      return Home();
-//    }
-  return SignIn();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
