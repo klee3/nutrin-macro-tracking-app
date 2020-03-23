56499e96ea2ff3df3f6d130bc115305863da960a
Ryan Mehri <ryan.mehri1@gmail.com>
2020-03-22 19:24:09 -0700

    Add stronger password validation

--- nutrin/lib/screens/authenticate/register.dart
+++ nutrin/lib/screens/authenticate/register.dart
@@ -25,79 +25,83 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.green[200],
-      appBar: AppBar(
-        backgroundColor: Colors.green[500],
-        elevation: 0.0,
-        title: Text('Register for Nutrin'),
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Sign in'))
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecorationEmail,
-                // TODO: should check using regex
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecorationPassword,
-                obscureText: true,
-                validator: (val) =>
-                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.blueGrey[400],
-                child: Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.green[200],
+            appBar: AppBar(
+              backgroundColor: Colors.green[500],
+              elevation: 0.0,
+              title: Text('Register for Nutrin'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text('Sign in'))
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: textInputDecorationEmail,
+                      // TODO: should check using regex
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: textInputDecorationPassword.copyWith(
+                          errorMaxLines: 2),
+                      obscureText: true,
+                      validator: (val) => val.length >= 8 &&
+                              val.contains(new RegExp(r"\d")) &&
+                              val.contains(new RegExp(r"[A-Z]"))
+                          ? null
+                          : 'Please enter a password 8+ characters long with at least one number and one uppercase letter.',
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.blueGrey[400],
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          await _authService
+                              .registerWithEmailAndPassword(email, password)
+                              .catchError((exception) => setState(() {
+                                    error =
+                                        "Email is invalid! Please try again.";
+                                    loading = false;
+                                  }));
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    setState(() => loading = true);
-                    dynamic result = await _authService
-                        .registerWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      // TODO: different error message for already exists etc.
-                      setState(() {
-                        error = 'Please supply a valid email';
-                        loading = false;
-                      });
-                    }
-                  }
-                },
               ),
-              SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- nutrin/lib/services/auth.dart
+++ nutrin/lib/services/auth.dart
@@ -31,44 +31,26 @@ class AuthService {
   // TODO: special error messages depending on what happened
   // sign in with email and password
   Future signInWithEmailAndPassword(String email, String password) async {
-    try {
-      print(email);
-      print(password);
-      AuthResult result = await _auth.signInWithEmailAndPassword(
-          email: email.trim(), password: password);
-      FirebaseUser firebaseUser = result.user;
-      return _userFromFirebaseUser(firebaseUser);
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
+    print(email);
+    print(password);
+    AuthResult result = await _auth.signInWithEmailAndPassword(
+        email: email.trim(), password: password);
+    FirebaseUser firebaseUser = result.user;
+    return _userFromFirebaseUser(firebaseUser);
   }
 
   // register with email and password
   Future registerWithEmailAndPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.createUserWithEmailAndPassword(
-          email: email.trim(), password: password);
-      FirebaseUser firebaseUser = result.user;
+    AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email.trim(), password: password);
+    FirebaseUser firebaseUser = result.user;
 
-      // create a new document for the user with uid
-      // TODO: should ask after registering
-      DatabaseService databaseService = DatabaseService(uid: firebaseUser.uid);
-      await databaseService.createNewUser(
-          "test user", true, "F", 1.6, 50, 18, 1.3, "lose");
-//      await DatabaseService(uid: firebaseUser.uid).updateMeals('breakfast', [
-//        TrackedFood('hecc', 1.0, 1.0, 1.0, 2.0, 3.0, 3.0, 4.0, 5.0, 67.0, 67.0,
-//            5.0, 69420.0)
-//      ]);
-//      await DatabaseService(uid: firebaseUser.uid).updateMeals('lunch', [
-//        TrackedFood('ahh', 2.0, 14.0, 14.0, 232.0, 33.0, 35.0, 47.0, 355.0,
-//            687.0, 67.0, 5.0, 6942011.0)
-//      ]);
-      return _userFromFirebaseUser(firebaseUser);
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
+    // create a new document for the user with uid
+    // TODO: should ask after registering
+    DatabaseService databaseService = DatabaseService(uid: firebaseUser.uid);
+    await databaseService.createNewUser(
+        "test user", true, "F", 1.6, 50, 18, 1.3, "lose");
+    return _userFromFirebaseUser(firebaseUser);
   }
 
   // sign out
