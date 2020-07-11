541fcebf20fb8e8f8bc5334fdc733e1f171f4f70
Lucy <hao.lucyy@gmail.com>
2020-07-10 20:19:32 -0600

    clean up

--- nutrin/lib/screens/wrapper.dart
+++ nutrin/lib/screens/wrapper.dart
@@ -1,27 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:mobileapp/main.dart';
 import 'package:mobileapp/model/user.dart';
-import 'package:mobileapp/screens/home/app.dart';
-import 'package:mobileapp/screens/home/meal_display.dart';
-import 'package:mobileapp/screens/home/test_meal_display.dart';
-import 'package:mobileapp/screens/user%20introduction/graph.dart';
-import 'package:mobileapp/screens/user%20introduction/new_sign_up_decision.dart';
-import 'package:mobileapp/screens/user%20introduction/veggies.dart';
 import 'package:provider/provider.dart';
+import 'authenticate/authenticate.dart';
+import 'home/settings.dart';
 
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   final user = Provider.of<User>(context);
+    final user = Provider.of<User>(context);
 
-   // return either Home or Authenticate widget
-  //  if (user == null) {
-  //    return Authenticate();
-  //  } else {
-  //    return Home();
-  //  }
-  return App();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
   
