6500683829dba722e3532f11f4fc43449aca7718
Lucy <hao.lucyy@gmail.com>
2020-07-12 11:23:31 -0600

    new files

---
+++ nutrin/lib/screens/main/settings/macrosform.dart
@@ -0,0 +1,83 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:mobileapp/model/tracker.dart';
+import 'package:mobileapp/model/user.dart';
+import 'package:mobileapp/services/database.dart';
+import 'package:provider/provider.dart';
+
+class MacroForm extends StatefulWidget {
+  @override
+  _MacroFormState createState() => _MacroFormState();
+}
+
+class _MacroFormState extends State<MacroForm> {
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var user = Provider.of<User>(context);
+    var tracker = Provider.of<Tracker>(context);
+
+    return Container(
+      child: Form(
+        key: _formkey,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(user.uid),
+                Text(tracker.personalNutrients.containsKey("Carbs")
+                    ? tracker.personalNutrients["carbs"].toString()
+                    : "0"),
+                macroTextFeild("Carbs"),
+                macroTextFeild("Protein"),
+                macroTextFeild("Fat"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    if (_formkey.currentState.validate()) {
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Macros have changed."),
+                        ),
+                      );
+                    }
+                  },
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Scaffold.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Macros have been set to default"),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget macroTextFeild(String name) {
+    return Column(
+      children: <Widget>[
+        Text(name),
+        TextFormField(
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Please enter a number.';
+            }
+            return null;
+          },
+        ),
+      ],
+    );
+  }
+}
---
+++ nutrin/lib/screens/main/settings/settings.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:mobileapp/screens/main/settings/macrosform.dart';
+import 'package:mobileapp/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:mobileapp/model/tracker.dart';
+import 'package:mobileapp/model/user.dart';
+
+class Settings extends StatelessWidget {
+  final AuthService _authService = AuthService();
+  final _formkey = GlobalKey<FormState>();
+  var carbs = 0;
+  var protein = 0;
+  var fat = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    var user = Provider.of<User>(context);
+    var tracker = Provider.of<Tracker>(context);
+
+    return Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.settings,
+            color: Colors.black,
+          ),
+          title: Text(
+            "Settings",
+            style: TextStyle(
+                fontSize: 20, color: Colors.black, fontFamily: "Comfortaa"),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async {
+                  await _authService.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text(
+                  'Log Out',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, fontFamily: "Comfortaa"),
+                ))
+          ],
+        ),
+        body: Form(
+          key: _formkey,
+          child: Container(
+            height: 200,
+            width: 400,
+            child: Column(
+              children: <Widget>[
+                Text(user.uid),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    macroTextFeild("carbohydrates", tracker),
+                    macroTextFeild("protein", tracker),
+                    macroTextFeild("fat", tracker),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    RaisedButton(
+                        child: Text("Set New Macros"),
+                        onPressed: () {
+                          if (_formkey.currentState.validate()) {
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Macros have changed."),
+                              ),
+                            );
+                          }
+                        }),
+                    RaisedButton(
+                        child: Text("Set To Default"),
+                        onPressed: () {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("Macros have been set to default"),
+                            ),
+                          );
+                        })
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget macroTextFeild(String name, Tracker tracker) {
+    return Container(
+      height: 100,
+      width: 100,
+      child: Column(
+        children: <Widget>[
+          Text(name),
+          TextFormField(
+            initialValue: tracker.personalNutrients.containsKey(name)
+                ? tracker.personalNutrients[name].round().toString()
+                : 0.toString(),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter a number.';
+              }
+              tracker.personalNutrients.containsKey(name)
+                  ? tracker.personalNutrients[name] = double.parse(value)
+                  : tracker.personalNutrients
+                      .putIfAbsent(name, () => double.parse(value));
+              return null;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
